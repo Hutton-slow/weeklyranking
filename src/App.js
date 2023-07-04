34b2e66e6e739471b8f78390aa46b5ab1acc5a0d
Asigbey Ezekiel Kofi <98233843+Hutton-slow@users.noreply.github.com>
2023-07-04 07:23:49 +0000

    Adding files
    Added files

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Board from "./components/board";
+import "./components/style.css";
+
+export default function App() {
+  return (
+    <div className="App" id="main">
+      <Board></Board>
+    </div>
+  );
+}
---
+++ src/components/database.js
@@ -0,0 +1,76 @@
+
+
+export const Leaderboard = [
+  {
+    name: "Ezekiel Kofi Asigbey",
+    class: "HND COMPUTER SCIENCE, L200",
+    score: "NA",
+    img: "", // Use the imported image
+    dt: "2023-07-01",
+  },
+  
+    {
+      name: "Emmanuel Tokoli",
+      class: "HND COMPUTER SCIENCE, L200",
+      score: "NA",
+      img: "",
+      dt: "2023-07-01"
+    },
+  
+    {
+      name: "Seth Kpotor Mawunya ",
+      class: "HND COMPUTER SCIENCE, L200",
+      score: "NA",
+      img: "",
+      dt: "2023-07-01"
+    },
+  
+    {
+      name: "Joseph Ahiakor",
+      class: "HND COMPUTER SCIENCE, L200",
+      score: "NA",
+      img: "",
+      dt: "2023-07-01"
+    },
+  
+    {
+      name: "Enoch Kwame",
+      class: "HND ICT, L200",
+      score: "NA",
+      img: "",
+      dt: "2023-07-01"
+    },
+  
+    {
+      name: "Nubor Kwame Eric",
+      class: "HND ICT, L200",
+      score: "NA",
+      img: "",
+      dt: "2023-07-01"
+    },
+  
+    {
+      name: "Akoto Ernest",
+      class: "BTECH ICT, L200",
+      score: "NA",
+      img: "",
+      dt: "2023-07-01"
+    },
+  
+    {
+      name: "Gati Godshand",
+      class: "HND ICT, L100",
+      score: "NA",
+      img: "",
+      dt: "2023-07-01"
+    },
+  
+    {
+      name: "Samuel Atsyatsya",
+      class: "BTECH ICT, L200",
+      score: "NA",
+      img: "",
+      dt: "2023-07-01"
+    }
+  ];
+  
